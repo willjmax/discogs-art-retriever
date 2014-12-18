f57f2279e4571cfaa125d272b492c95a5455329c
zahl- <wmaxwell90@gmail.com>
2014-12-18 13:59:01 -0600

    minor bug fix

--- fetchart.py
+++ fetchart.py
@@ -33,11 +33,10 @@ else:
 		tag = eyed3.load(args['path'] + "/" + name)
 		artist = tag.tag.artist
 		release = tag.tag.album
-		resp, content = discogs.client.request("https://api.discogs.com/database/search?release_title={0}&artist={1}".format(release, artist), headers={'User-Agent': discogs.user_agent})
-		json_result = json.loads(content)
-		resp, image = discogs.client.request(json_result['results'][0]['thumb'], headers={'User-Agent': discogs.user_agent})
-		with open(args['path'] + '/folder.jpg', 'w') as fh:
-			fh.write(image)	
+	resp, content = discogs.client.request("https://api.discogs.com/database/search?release_title={0}&artist={1}".format(release, artist), headers={'User-Agent': discogs.user_agent})
+	json_result = json.loads(content)		resp, image = discogs.client.request(json_result['results'][0]['thumb'], headers={'User-Agent': discogs.user_agent})
+	with open(args['path'] + '/folder.jpg', 'w') as fh:
+		fh.write(image)	
 
 
 
